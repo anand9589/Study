af142717d587d932f80ade2da9fa29916a35f4f5
anand9589 <anand9589@gmail.com>
2022-03-16 17:49:31 -0500

    String Problem for contains

---
+++ facebook/FacenookProblem/StringProblme/Program.cs
@@ -0,0 +1,25 @@
+﻿string str1 = "abcabcabcd";
+string str2 = "abcd";
+
+int result = IsContains(str1, str2);
+Console.WriteLine(result);
+int IsContains(string s, string x)
+{
+    for (int i = 0; i <= s.Length - x.Length; i++)
+    {
+        if(s[i] == x[0])
+        {
+            bool isMatch = true;
+            for (int j = 1; j < x.Length; j++)
+            {
+                if(x[j] != s[i + j])
+                {
+                    isMatch = false;
+                    break;
+                }
+            }
+            if (isMatch) return i;
+        }
+    }
+    return -1;
+}
