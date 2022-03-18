df36e84f2f14a144ddb9d0971b070f4b68432201
anand9589 <anand9589@gmail.com>
2022-03-18 16:11:26 -0500

    Added Activity Selection Base

---
+++ facebook/FacenookProblem/ActivitySelectionProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] start = { 1, 3, 0, 5, 8, 5 };
+int[] end = { 2, 4, 6, 7, 9, 9 };
+
+int[] result = activitySelection(start, end);
+
+int[] activitySelection(int[] start, int[] end)
+{
+    for (int i = 0; i < start.Length; i++)
+    {
+
+    }
+    return new int[2];
+}
