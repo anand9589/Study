a775d1722f0b72f61c62aae96030e0f0824f56a3
anand9589 <anand9589@gmail.com>
2022-03-26 14:20:40 -0500

    Added LinkedListStack problem

---
+++ Problems/StackProblems/Classes/LinkedListStack.cs
@@ -0,0 +1,39 @@
+﻿using Common;
+
+namespace StackProblems.Classes
+{
+    public class LinkedListStack
+    {
+        Node head = null;
+        public void push(int x)
+        {
+            if (head == null)
+            {
+                head = new Node(x);
+            }
+            else
+            {
+                Node temp = head;
+                Node newNode = new Node(x);
+                newNode.Next = temp;
+                head = newNode;
+            }
+        }
+
+        public int pop()
+        {
+            if (head == null) return -1;
+            int data  = head.Data;
+            if(head.Next != null)
+            {
+                Node temp = head.Next;
+                head = temp;
+            }
+            else
+            {
+                head = null;
+            }
+            return data;
+        }
+    }
+}
