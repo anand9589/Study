5eae2575eccb00a8fef3df7f461d79519b4f7e8c
anand9589 <anand9589@gmail.com>
2022-03-26 12:24:33 -0500

    Added basic stacks using array

---
+++ Problems/StackProblems/Classes/MyStack.cs
@@ -0,0 +1,27 @@
+﻿namespace StackProblems.Classes
+{
+    public class MyStack
+    {
+        private int top = -1;
+        private readonly int size = 100;
+        private int[] arr;
+        public MyStack()
+        {
+            arr = new int[size];
+        }
+        public void push(int x)
+        {
+            if (top == size - 1) return;
+            top++;
+            arr[top] = x;
+        }
+        public int pop()
+        {
+            if (top == -1) return -1;
+            int data = arr[top];
+            top--;
+            return data;
+        }
+
+    }
+}
