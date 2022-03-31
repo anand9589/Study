252db7b837af37b63b9303e453e40a921492b8e2
anand9589 <anand9589@gmail.com>
2022-03-31 13:56:59 -0500

    Added CircularTour

--- Problems/QueueProblems/Program.cs
+++ Problems/QueueProblems/Program.cs
@@ -1,34 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using QueueProblems;
 using QueueProblems.LRU;
 
 Console.WriteLine("Hello, World!");
-var lines = File.ReadAllLines(@"C:\Workspace\Study\Problems\LRUCache.txt");
+var lines = File.ReadAllLines(@"C:\Workspace\Study\Problems\CircularTour.txt");
 
 #region LRUCache
-int cap = int.Parse(lines[0]);
-int queries = int.Parse(lines[1]);
-LRUCache_V1 lRUCache = new LRUCache_V1(cap);
+//int cap = int.Parse(lines[0]);
+//int queries = int.Parse(lines[1]);
+//LRUCache_V1 lRUCache = new LRUCache_V1(cap);
 
-string[] strings = lines[2].Trim().Split(' ');
+//string[] strings = lines[2].Trim().Split(' ');
 
-for (int i = 0; i < strings.Length; i++)
+//for (int i = 0; i < strings.Length; i++)
+//{
+//    switch (strings[i])
+//    {
+//        case "GET":
+//            i++;
+//            lRUCache.get(int.Parse(strings[i]));
+//            break;
+//        case "SET":
+//            i++;
+//            int key = int.Parse(strings[i]);
+//            i++;
+//            int value = int.Parse(strings[i]);
+//            lRUCache.set(key, value);
+//            break;
+//        default:
+//            break;
+//    }
+//}
+
+#endregion
+
+Solution solution = new Solution();
+#region CircularTour
+int n = int.Parse(lines[0]);
+int[] data = Array.ConvertAll(lines[1].Trim().Split(' '), int.Parse);
+
+int[] p = new int[n];
+int[] d = new int[n];
+
+for (int i = 0; i < n; i++)
 {
-    switch (strings[i])
-    {
-        case "GET":
-            i++;
-            lRUCache.get(int.Parse(strings[i]));
-            break;
-        case "SET":
-            i++;
-            int key = int.Parse(strings[i]);
-            i++;
-            int value = int.Parse(strings[i]);
-            lRUCache.set(key, value);
-            break;
-        default:
-            break;
-    }
+    p[i] = data[i*2];
+    d[i] = data[i*2 +1];
 }
-
+Console.WriteLine(String.Join(',', p));
+Console.WriteLine(String.Join(',', d));
+var res = global::QueueProblems.Solution.Tour(p, d, n);
+Console.WriteLine(res);
 #endregion
--- Problems/QueueProblems/Solution.cs
+++ Problems/QueueProblems/Solution.cs
@@ -2,22 +2,53 @@
 {
     internal class Solution
     {
-        public int Tour(int[] p, int[] d, int n)
+        public static int Tour(int[] p, int[] d, int n)
         {
             int balance = 0;
+            int petrolSum = 0;
+            int distanceSum = 0;
             //your Code here
             Queue<int> q = new Queue<int>();
 
             for (int i = 0; i < n; i++)
             {
-                if(p[i] >= d[i] + balance)
+                petrolSum += p[i];
+                distanceSum+= d[i];
+
+                if(p[i] + balance >= d[i])
                 {
-                    balance += p[i]-d[i];
+                    balance = balance + p[i]-d[i];
                     q.Enqueue(i);
+                }
+                else
+                {
+                    balance = 0;
+                    q.Clear();
                 }
             }
 
-            return 0;
+            if (q.Count == 0 || petrolSum<distanceSum) return -1;  
+
+            if (q.First() == 0 && q.Last() == n - 1) return q.First();
+
+            for (int i = 0; i < q.First(); i++)
+            {
+                if (p[i] + balance >= d[i])
+                {
+                    balance = balance + p[i] - d[i];
+                    q.Enqueue(i);
+                }
+                else
+                {
+                    while (q.Count != 0)
+                    {
+                        q.Dequeue();
+                    }
+                }
+            }
+
+
+            return q.Count == n ? q.First() : -1;
         }
     }
 }
