5e0f777ccc8a21d831126c10c5eaeed9bdb514b4
anand9589 <anand9589@gmail.com>
2022-03-31 15:00:00 -0500

    Added QueueByLinkedList

---
+++ Problems/QueueProblems/QueueByLinkedList.cs
@@ -0,0 +1,33 @@
+﻿namespace QueueProblems
+{
+    internal class QueueByLinkedList
+    {
+        QueueNode Head = null;
+        public void push(int x)
+        {
+            if(Head == null)
+            {
+                Head = new QueueNode(x);
+            }
+            else
+            {
+                var temp = Head;
+
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+
+                temp.next = new QueueNode(x);
+            }
+        }
+
+        public int pop()
+        {
+            if(Head == null) return -1;
+            var res = Head.data;
+            Head = Head.next;
+            return res;
+        }
+    }
+}
---
+++ Problems/QueueProblems/QueueNode.cs
@@ -0,0 +1,14 @@
+﻿namespace QueueProblems
+{
+    internal class QueueNode
+    {
+        public int data;
+        public QueueNode next;
+
+        public QueueNode(int a)
+        {
+            this.data = a;
+            this.next = null;
+        }
+    }
+}
