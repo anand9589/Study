bc8d9147b206dfcbda33beaa3e2adeff0a69d1df
anand9589 <anand9589@gmail.com>
2022-04-19 19:19:18 -0500

    Added BSTIterator

---
+++ Problems/TreeProblem/BSTIterator.cs
@@ -0,0 +1,32 @@
+﻿namespace GraphProject
+{
+    internal class BSTIterator
+    {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        public BSTIterator(TreeNode root)
+        {
+            partialInorder(root);
+        }
+
+        private void partialInorder(TreeNode root)
+        {
+            while (root != null)
+            {
+                stack.Push(root);
+                root = root.left;
+            }
+        }
+
+        public int Next()
+        {
+            TreeNode treeNode = stack.Pop();
+            partialInorder((TreeNode)treeNode.right);
+            return treeNode.val;
+        }
+
+        public bool HasNext()
+        {
+            return stack.Count > 0;
+        }
+    }
+}
