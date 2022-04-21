3b7286deb90b85d497efb8808a458ac0037a77a4
anand9589 <anand9589@gmail.com>
2022-04-21 13:51:51 -0500

    Added MyHashSet

---
+++ Problems/ArrayProblems/MyHashSet.cs
@@ -0,0 +1,29 @@
+﻿namespace ArrayProblems
+{
+    internal class MyHashSet
+    {
+        private List<int> list;
+        public MyHashSet()
+        {
+            list = new List<int>();
+        }
+
+        public void Add(int key)
+        {
+            if (!Contains(key))
+            {
+                list.Add(key);
+            }
+        }
+
+        public void Remove(int key)
+        {
+            list.Remove(key);
+        }
+
+        public bool Contains(int key)
+        {
+            return list.Contains(key);
+        }
+    }
+}
