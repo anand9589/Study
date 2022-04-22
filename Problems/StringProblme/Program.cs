f204740a0686a58114ce1521878a509263c4d7f6
anand9589 <anand9589@gmail.com>
2022-04-22 16:22:51 -0500

    Fixed typo added solution class

--- Problems/StringProblme/Program.cs
+++ Problems/StringProblme/Program.cs
@@ -1,25 +1,8 @@
-﻿string str1 = "abcabcabcd";
+﻿using StringProblems;
+
+string str1 = "abcabcabcd";
 string str2 = "abcd";
 
-int result = IsContains(str1, str2);
+Solution solution = new Solution();
+int result = solution.IsContains(str1, str2);
 Console.WriteLine(result);
-int IsContains(string s, string x)
-{
-    for (int i = 0; i <= s.Length - x.Length; i++)
-    {
-        if(s[i] == x[0])
-        {
-            bool isMatch = true;
-            for (int j = 1; j < x.Length; j++)
-            {
-                if(x[j] != s[i + j])
-                {
-                    isMatch = false;
-                    break;
-                }
-            }
-            if (isMatch) return i;
-        }
-    }
-    return -1;
-}
---
+++ Problems/StringProblme/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace StringProblems
+{
+    internal class Solution
+    {
+
+        public int IsContains(string s, string x)
+        {
+            for (int i = 0; i <= s.Length - x.Length; i++)
+            {
+                if (s[i] == x[0])
+                {
+                    bool isMatch = true;
+                    for (int j = 1; j < x.Length; j++)
+                    {
+                        if (x[j] != s[i + j])
+                        {
+                            isMatch = false;
+                            break;
+                        }
+                    }
+                    if (isMatch) return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
