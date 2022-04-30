5fec20499e07edc1fed7834a5a68f6f1aff68e0d
anand9589 <anand9589@gmail.com>
2022-04-29 23:24:55 -0500

    refactore

--- Problems/LeetCode/Solution.cs
+++ Problems/LeetCode/Solution.cs
@@ -1,34 +1,7 @@
 ﻿using System.Text;
 
 namespace LeetCode
-{
-
-    class UF
-    {
-        private int[] parent;
-
-        public UF(int size)
-        {
-            parent = Enumerable.Range(0, size).ToArray();
-        }
-
-        public void Union(int x, int y)
-        {
-            int px = Find(x);
-            int py = Find(y);
-
-
-            if (px != py)
-            {
-                parent[px] = y;
-            }
-        }
-
-        public int Find(int x)
-        {
-            return parent[x] == x ? x : Find(parent[x]);
-        }
-    }
+{    
     public class Solution
     {
         #region #38CountAndSay
@@ -81,138 +54,6 @@ namespace LeetCode
             return stringBuilder.ToString();
         }
         #endregion
-
-        #region #1423 MaxScore
-        /// <summary>
-        /// LeetCode #1423
-        /// </summary>
-        /// <param name="cardPoints"></param>
-        /// <param name="k"></param>
-        /// <returns></returns>
-        public int MaxScore(int[] cardPoints, int k)
-        {
-            int sum = cardPoints.Sum();
-            int length = cardPoints.Length;
-
-            if (length == k) return sum;
-
-            int frame = 0;
-            int res = 0;
-
-            for (int i = 0; i < length - k - 1; i++)
-            {
-                frame += cardPoints[i];
-            }
-
-            for (int i = length - k - 1; i < length; i++)
-            {
-                frame += cardPoints[i];
-
-                res = Math.Max(res, sum - frame);
-
-                frame -= cardPoints[i - (length - k - 1)];
-            }
-
-            return res;
-        }
-        #endregion
-
-
-        #region Bipartite Graph
-        /// <summary>
-        /// Union find
-        /// </summary>
-        /// <param name="graph"></param>
-        /// <returns></returns>
-        public bool IsBipartite_V3(int[][] graph)
-        {
-            UF uf = new UF(graph.Length);
-
-            for (int i = 0; i < graph.Length; i++)
-            {
-                foreach (var item in graph[i])
-                {
-                    if (uf.Find(item) == uf.Find(i)) return false;
-
-                    uf.Union(item, graph[i][0]);
-                }
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// BFS, Graph Coloring By Queue
-        /// </summary>
-        /// <param name="graph"></param>
-        /// <returns></returns>
-        public bool IsBipartite_V2(int[][] graph)
-        {
-            int[] color = Enumerable.Repeat(0, graph.Length).ToArray();
-
-            for (int i = 0; i < graph.Length; i++)
-            {
-                if (color[i] == 0)
-                {
-                    color[i] = 1;
-                    Queue<int> q = new Queue<int>();
-                    q.Enqueue(i);
-
-                    while (q.Count > 0)
-                    {
-                        int n = q.Dequeue();
-
-                        foreach (var item in graph[n])
-                        {
-                            if (color[n] == color[item]) return false;
-                            else if (color[item] == 0)
-                            {
-                                q.Enqueue(item);
-                                color[item] = -color[n];
-                            }
-                        }
-                    }
-                }
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// DFS, Graph Coloring
-        /// 0 - No
-        /// -1 - Red
-        /// 1 - Blue
-        /// </summary>
-        /// <param name="graph"></param>
-        /// <returns></returns>
-        public bool IsBipartite_V1(int[][] graph)
-        {
-            int[] color = Enumerable.Repeat(0, graph.Length).ToArray();
-            for (int i = 0; i < graph.Length; i++)
-            {
-                if (color[i] == 0 && !validColor(graph, color, 1, i)) return false;
-            }
-            return true;
-        }
-
-        private bool validColor(int[][] graph, int[] color, int c, int node)
-        {
-            if (color[node] != 0)
-            {
-                return color[node] == c;
-            }
-
-            color[node] = c;
-
-            foreach (var item in graph[node])
-            {
-                if (!validColor(graph, color, -c, item)) return false;
-            }
-
-            return true;
-        }
-
-        #endregion
-
 
         #region #39 CombinationSum
 
@@ -460,6 +301,136 @@ namespace LeetCode
             }
         }
 
+
+        #endregion
+
+        #region #1423 MaxScore
+        /// <summary>
+        /// LeetCode #1423
+        /// </summary>
+        /// <param name="cardPoints"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int MaxScore(int[] cardPoints, int k)
+        {
+            int sum = cardPoints.Sum();
+            int length = cardPoints.Length;
+
+            if (length == k) return sum;
+
+            int frame = 0;
+            int res = 0;
+
+            for (int i = 0; i < length - k - 1; i++)
+            {
+                frame += cardPoints[i];
+            }
+
+            for (int i = length - k - 1; i < length; i++)
+            {
+                frame += cardPoints[i];
+
+                res = Math.Max(res, sum - frame);
+
+                frame -= cardPoints[i - (length - k - 1)];
+            }
+
+            return res;
+        }
+        #endregion
+
+        #region Bipartite Graph
+        /// <summary>
+        /// Union find
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <returns></returns>
+        public bool IsBipartite_V3(int[][] graph)
+        {
+            UF uf = new UF(graph.Length);
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                foreach (var item in graph[i])
+                {
+                    if (uf.Find(item) == uf.Find(i)) return false;
+
+                    uf.Union(item, graph[i][0]);
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// BFS, Graph Coloring By Queue
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <returns></returns>
+        public bool IsBipartite_V2(int[][] graph)
+        {
+            int[] color = Enumerable.Repeat(0, graph.Length).ToArray();
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                if (color[i] == 0)
+                {
+                    color[i] = 1;
+                    Queue<int> q = new Queue<int>();
+                    q.Enqueue(i);
+
+                    while (q.Count > 0)
+                    {
+                        int n = q.Dequeue();
+
+                        foreach (var item in graph[n])
+                        {
+                            if (color[n] == color[item]) return false;
+                            else if (color[item] == 0)
+                            {
+                                q.Enqueue(item);
+                                color[item] = -color[n];
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// DFS, Graph Coloring
+        /// 0 - No
+        /// -1 - Red
+        /// 1 - Blue
+        /// </summary>
+        /// <param name="graph"></param>
+        /// <returns></returns>
+        public bool IsBipartite_V1(int[][] graph)
+        {
+            int[] color = Enumerable.Repeat(0, graph.Length).ToArray();
+            for (int i = 0; i < graph.Length; i++)
+            {
+                if (color[i] == 0 && !validColor(graph, color, 1, i)) return false;
+            }
+            return true;
+        }
+
+        private bool validColor(int[][] graph, int[] color, int c, int node)
+        {
+            if (color[node] != 0)
+            {
+                return color[node] == c;
+            }
+
+            color[node] = c;
+
+            foreach (var item in graph[node])
+            {
+                if (!validColor(graph, color, -c, item)) return false;
+            }
+
+            return true;
+        }
 
         #endregion
     }
---
+++ Problems/LeetCode/UF.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode
+{
+    public class UF
+    {
+        private int[] parent;
+
+        public UF(int size)
+        {
+            parent = Enumerable.Range(0, size).ToArray();
+        }
+
+        public void Union(int x, int y)
+        {
+            int px = Find(x);
+            int py = Find(y);
+
+
+            if (px != py)
+            {
+                parent[px] = y;
+            }
+        }
+
+        public int Find(int x)
+        {
+            return parent[x] == x ? x : Find(parent[x]);
+        }
+    }
+}
