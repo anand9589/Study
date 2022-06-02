f531780360123f84281f57a1f941db3455e85649
anand9589 <anand9589@gmail.com>
2022-06-01 20:44:06 -0500

    867. Transpose Matrix

--- Problems/LeetCode/Solution.cs
+++ Problems/LeetCode/Solution.cs
@@ -4138,6 +4138,23 @@ namespace LeetCode
         }
         #endregion
 
+        #region 867. Transpose Matrix
+        public int[][] Transpose(int[][] matrix)
+        {
+            int[][] result = new int[matrix[0].Length][];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = new int[matrix.Length];
+                for (int j = 0; j < result[i].Length; j++)
+                {
+                   result[i][j] = matrix[j][i];
+                }
+            }
+            return result;
+        }
+        #endregion
+
         #region 876. Middle of the Linked List
         public ListNode MiddleNode(ListNode head)
         {
