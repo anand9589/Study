6684a7c70dd2f07427aa2b934bdd5977b8eb4160
anand9589 <anand9589@gmail.com>
2022-06-04 21:17:33 -0500

    final

--- Problems/LeetCode/Solution.cs
+++ Problems/LeetCode/Solution.cs
@@ -2884,6 +2884,31 @@ namespace LeetCode
         }
         #endregion
 
+        #region  304. Range Sum Query 2D - Immutable
+        public class NumMatrix
+        {
+            private int[][] matrix;
+            public NumMatrix(int[][] matrix)
+            {
+                this.matrix = matrix;
+            }
+
+            public int SumRegion(int row1, int col1, int row2, int col2)
+            {
+                int sum = 0;
+                for (int i = row1; i < row2; i++)
+                {
+                    for (int j = col1; j < col2; j++)
+                    {
+                        sum += matrix[i][j];
+                    }
+                }
+                return sum;
+            }
+        }
+
+        #endregion
+
         #region 318. Maximum Product of Word Lengths
 
         public int MaxProduct(string[] words)
